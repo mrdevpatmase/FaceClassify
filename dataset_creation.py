ec9028cdf601e1b2dc5307173ed8885a2c94e4be
Dev Patmase <devpatmase1@gmail.com>
2025-07-26 01:30:50 +0530

    Create dataset file

---
+++ dataset_creation.py
@@ -0,0 +1,12 @@
+from icrawler.builtin import GoogleImageCrawler
+
+def download_images(query, folder_name, max_num=50):
+    google_crawler = GoogleImageCrawler(storage={'root_dir': f'dataset/{folder_name}'})
+    google_crawler.crawl(keyword=query, max_num=max_num)
+
+# Cricketer face image downloads
+download_images("Virat Kohli face", "virat_kohli", 50)
+download_images("MS Dhoni face", "ms_dhoni", 50)
+download_images("AB de Villiers face", "ab_de_villiers", 50)
+download_images("Jasprit Bumrah face", "jasprit_bumrah", 50)
+download_images("Ruturaj Gaikwad face", "ruturaj_gaikwad", 50)
