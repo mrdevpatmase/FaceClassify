77083aa69b1228a9c00cf2599a538548651fc9e7
Dev Patmase <devpatmase1@gmail.com>
2025-07-27 00:36:34 +0530

    Update dataset creation

--- dataset_creation.py
+++ dataset_creation.py
@@ -5,8 +5,8 @@ def download_images(query, folder_name, max_num=50):
     google_crawler.crawl(keyword=query, max_num=max_num)
 
 # Cricketer face image downloads
-download_images("Virat Kohli face", "virat_kohli", 50)
-download_images("MS Dhoni face", "ms_dhoni", 50)
-download_images("AB de Villiers face", "ab_de_villiers", 50)
-download_images("Jasprit Bumrah face", "jasprit_bumrah", 50)
-download_images("Ruturaj Gaikwad face", "ruturaj_gaikwad", 50)
+download_images("Virat Kohli face", "virat_kohli", 200)
+download_images("MS Dhoni face", "ms_dhoni", 200)
+download_images("AB de Villiers face", "ab_de_villiers", 200)
+download_images("Jasprit Bumrah face", "jasprit_bumrah", 200)
+download_images("Ruturaj Gaikwad face", "ruturaj_gaikwad", 200)
